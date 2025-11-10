8abf0b913d5f36567735abcfe610c6e115c94d87
e.petukhova <e.petukhova@yadro.com>
2025-11-10 18:02:24 +0300

    homework17

---
+++ src/main/kotlin/lessons/lesson17/homeworks/SlotMachine.kt
@@ -0,0 +1,17 @@
+package lessons.lesson17.homeworks
+
+class SlotMachine {
+    val color: String? = null
+    val model:String
+    var isTurnOn: Boolean = false
+    var isOs: Boolean = false
+    val gameList: List<String>
+    val isJoystick
+
+
+    fun turnOn(isTurnOn: Boolean){
+
+    }
+
+
+}
